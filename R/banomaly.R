34d6cfb964bba45c6b62b7e629dc974a963bf6a5
yanjzhou <yanjzhou@ebay.com>
2015-10-13 06:43:04 +0800

    up

--- R/banomaly.R
+++ R/banomaly.R
@@ -1,9 +1,14 @@
 
+library(AnomalyDetection)
+
 banomaly <- function(input){
     newdata <- if(is.character(input) && file.exists(input)){
         read.csv(input)
     } else {
         as.data.frame(input)
     }
-    
+
+    data(raw_data)
+    res = AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
+    return(res$anoms)
 }
