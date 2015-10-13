6b1b4aefa0f80e169710c6e317b4f3d3b5cca039
yanjzhou <yanjzhou@ebay.com>
2015-10-13 16:07:45 +0800

    up

--- R/banomaly.R
+++ R/banomaly.R
@@ -12,8 +12,8 @@ banomaly <- function(input){
 #    res = AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
 #    res = AnomalyDetectionTs(newdata1, max_anoms=0.02, direction='both', plot=FALSE)
 
-    return(newdata1);
+#    return(newdata1);
     
-    res = AnomalyDetectionTs(newdata1$data, max_anoms=newdata1$max_anoms, direction='both', plot=FALSE)
+    res = AnomalyDetectionTs(newdata1, max_anoms=input$max_anoms, direction='both', plot=FALSE)
     return(res$anoms)
 }
