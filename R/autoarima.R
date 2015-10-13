fe22aea296e6b914a672e5d4c8893c721d418855
yanjzhou <yanjzhou@ebay.com>
2015-10-14 07:16:15 +0800

    up

---
+++ R/autoarima.R
@@ -0,0 +1,36 @@
+#' Score the TV watching model
+#' 
+#' Simple model with two predictor \code{age} (value between 20 and 80) and \code{marital} 
+#' ("MARRIED", 'DIVORCED", "WIDOWED or "NEVER MARRIED").
+#' 
+#' @export
+#' @importFrom mgcv predict.gam
+#' @param input data passed on as \code{newdata} to \code{\link{predict}}
+#' @examples mydata <- data.frame(
+#'    age=c(24, 54, 32, 75),
+#'    marital=c("MARRIED", "DIVORCED", "WIDOWED", "NEVER MARRIED")
+#' )
+#' tv(mydata)
+
+#require("astsa");
+#library(forecast);
+
+autoarima <- function(input){
+                                        #input can either be csv file or data	
+    newdata <- if(is.character(input) && file.exists(input)){
+        read.csv(input)
+    } else {
+        as.data.frame(input)
+    }
+
+    library(forecast)
+
+    kings <- newdata;
+
+                                        #    kingstimeseries <- ts(kings)
+    kingst <- auto.arima(kings);
+    
+    res = predict(kingst, h=10);
+
+    return(res$mean);
+}
