aec44e9e3ee9cc084830de23f616fbd9101ee7bc
yanjzhou <yanjzhou@ebay.com>
2015-11-15 22:42:23 -0800

    up

--- R/anomaly_batch.R
+++ R/anomaly_batch.R
@@ -12,7 +12,7 @@ anomalybatch <- function(input){
 #    res = AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
 #    res = AnomalyDetectionTs(newdata1, max_anoms=0.02, direction='both', plot=FALSE)
 
-#    return(newdata1);
+    return(newdata1);
     
     res = AnomalyDetectionTs(newdata1, max_anoms=input$max_anoms, direction='both', plot=FALSE)
     
