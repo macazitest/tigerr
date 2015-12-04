10fb8e48dbafa20f37df1b7c503fd685247c14f9
yanjzhou <yanjzhou@ebay.com>
2015-12-04 08:02:37 -0800

    remove unneeded

--- R/anomalybatch.R
+++ R/anomalybatch.R
@@ -1,11 +1,6 @@
 
 anomalybatch <- function(input){
-    newdata1 <- if(is.character(input) && file.exists(input)){
-        read.csv(input)
-    } else {
-        as.data.frame(input$data)
-    }
-
+    
     dataobj <- input$data;
 
     dims <- attributes(dataobj);
@@ -24,18 +19,5 @@ anomalybatch <- function(input){
     }
 
     return(reslist);
-
-                                        #    return(newdata1)
     
-#    data(raw_data)
-#    res = AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
-#    res = AnomalyDetectionTs(newdata1, max_anoms=0.02, direction='both', plot=FALSE)
-
-    return(input);
-
-    return(newdata1);
-    
-    res = AnomalyDetectionTs(newdata1, max_anoms=input$max_anoms, direction='both', plot=FALSE)
-    
-    return(res$anoms)
 }
